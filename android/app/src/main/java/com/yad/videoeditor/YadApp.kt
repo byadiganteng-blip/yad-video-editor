5da30b0bc0798c4cdec042acd0d9aff6f270308f
byadiganteng-blip <byadiganteng@gmail.com>
2026-09-12 23:57:01 +0700

    feat: YadApp register user ke Firebase saat start

--- android/app/src/main/java/com/yad/videoeditor/YadApp.kt
+++ android/app/src/main/java/com/yad/videoeditor/YadApp.kt
@@ -3,15 +3,12 @@ package com.yad.videoeditor
 import android.app.Application
 import android.util.Log
 
-/**
- * Application class
- * Created by KARYADI, Coding by KARYADI
- */
 class YadApp : Application() {
     override fun onCreate() {
         super.onCreate()
         try {
             SecureConfig.init(this)
+            FirebaseManager.registerUser(this)
             Log.d("YadApp", "Initialized")
         } catch (e: Exception) {
             Log.e("YadApp", "Init failed", e)
