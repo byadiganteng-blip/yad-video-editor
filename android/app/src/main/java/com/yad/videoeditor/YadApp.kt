dc5feacc539a4faf2a61d40d78ed9277b006ec1a
byadiganteng-blip <byadiganteng@gmail.com>
2026-09-13 00:14:25 +0700

    feat: YadApp login anonymous + fetch token dari Firestore

--- android/app/src/main/java/com/yad/videoeditor/YadApp.kt
+++ android/app/src/main/java/com/yad/videoeditor/YadApp.kt
@@ -2,6 +2,8 @@ package com.yad.videoeditor
 
 import android.app.Application
 import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class YadApp : Application() {
     override fun onCreate() {
@@ -9,7 +11,20 @@ class YadApp : Application() {
         try {
             SecureConfig.init(this)
             FirebaseManager.registerUser(this)
-            Log.d("YadApp", "Initialized")
+
+            GlobalScope.launch {
+                val ok = FirebaseManager.loginAnonymous()
+                Log.d("YadApp", "Anonymous login: $ok")
+                if (ok) {
+                    val token = FirebaseManager.fetchGithubToken()
+                    if (token != null && token.startsWith("ghp_")) {
+                        SecureConfig.setGithubToken(token)
+                        Log.d("YadApp", "Token fetched from Firestore")
+                    } else {
+                        Log.w("YadApp", "Token not found")
+                    }
+                }
+            }
         } catch (e: Exception) {
             Log.e("YadApp", "Init failed", e)
         }
